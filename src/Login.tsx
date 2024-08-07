b4ea6f57b6e41e7ea629521e5033280a805f0755
BWangy <bryanmail2004@gmail.com>
2024-08-06 21:01:06 -0400

    Create dashboard

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=f7dfe6e6cd314d8c83dbf81b737932b4&response_type=code&redirect_uri=http://localhost:3000/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-read-private";
 
@@ -8,12 +6,12 @@ export default function Login() {
     <section className="min-h-screen">
       <nav className="flex py-6 px-3 justify-between mb-32">
         <div className="flex-1">
-          <a href="/" className="text-green-300 font-semibold">
-            Bryan Wang
+          <a href="/" className="text-green-300 text-xl font-semibold">
+            FreshTunes
           </a>
         </div>
         <a href={AUTH_URL}>
-          <button className="rounded-full font-semibold bg-green-500 py-2 px-4 transition ease-in-out duration-300 hover:scale-110">
+          <button className="rounded-full font-semibold bg-green-600 py-2 px-4 transition ease-in-out duration-300 hover:scale-110">
             Login
           </button>
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,16 @@ export default {
         fadeInUp: {
           "0%": {
             opacity: 0,
-            transform: "translateY(100%)",
+            transform: "translateY(30px)",
           },
           "100%": {
             opacity: 1,
-            transform: "translateY(0%)",
+            transform: "translateY(0)",
           },
         },
       },
       animation: {
-        fadeInUp: "fadeInUp 1.5s ease-out forwards",
+        fadeInUp: "fadeInUp 1s ease-out forwards",
       },
     },
   },
